e5af4572228a4cee0fe7ae3e325b82418644778b
Louis Chou <louis6110xx@gmail.com>
2023-10-29 02:18:43 +0800

    migrate

--- database/migrations/2023_10_28_174121_user_table.php
+++ database/migrations/2023_10_28_174121_user_table.php
@@ -11,7 +11,34 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('orgs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('org_no');
+            $table->time('created_at');
+            $table->time('updated_at');
+        });
+
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('org_id');
+            $table->string('name');
+            $table->date('birthday');
+            $table->string('email');
+            $table->string('account');
+            $table->string('password');
+            $table->number('status');
+            $table->time('created_at');
+            $table->time('updated_at');
+        });
+
+        Schema::create('apply_file', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->string('file_path');
+            $table->time('created_at');
+            $table->time('updated_at');
+        });
     }
 
     /**
